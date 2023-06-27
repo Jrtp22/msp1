0a1136c2567cf29fe86695c97904b377e98c7adb
jrtp22 <jrtp22@gmail.com>
2023-06-26 21:09:03 -0700

    got it to display the random word without showing it and mapped out the function I will need for the game to begin

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ this is going to be a Hangman game where you have to hack(guess) the word to sto
 
 
 https://stackoverflow.com/questions/48977848/how-do-i-create-different-button-for-each-item-in-an-array-javascript
+
+https://www.geeksforgeeks.org/how-to-select-a-random-element-from-array-in-javascript/
+
+https://dmitripavlutin.com/javascript-fill-array/#21-using-arrayfill
+
+https://herewecode.io/blog/array-to-string-without-commas-javascript/
--- app.js
+++ app.js
@@ -6,4 +6,18 @@ letters.forEach(letter => {
     letterBtn.setAttribute("id", letter);
     letterBoard.appendChild(letterBtn);
 });
-
+let maxLives = 10;
+let gameWords = ['malware', 'phishing', 'spam', 'spyware', 'trojan', 'abcdefg', 'password', 'passcode', 'hacking'];
+let selectedWord = gameWords[Math.floor(Math.random() * gameWords.length)];
+let hiddenWord = Array(selectedWord.length).fill('_');
+let wordDisplay = document.getElementById('wordDisplay');
+wordDisplay.innerHTML = hiddenWord.join(" ");
+console.log(selectedWord);
+//add function to start game
+//add function to check if the letter is in the word
+//add function to minus life if wrong and end game at 0
+//add function to add letter to the word if correct
+//add function to check when the word is complete and invoke a win function
+//add a win function
+//add function of end game to call above when game is at 0
+//add function to reset game
